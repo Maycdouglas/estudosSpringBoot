e7eab0081b5fd3d5ef776b175d40e59126d8a646
Maycdouglas <mayconcruzeiroraposa@gmail.com>
2025-09-19 08:25:37 -0300

    inicializa projeto spring boot

---
+++ src/main/java/br/com/estudos_spring_boot/estudos_spring_boot/EstudosSpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.com.estudos_spring_boot.estudos_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudosSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudosSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/estudos_spring_boot/estudos_spring_boot/EstudosSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.estudos_spring_boot.estudos_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudosSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
